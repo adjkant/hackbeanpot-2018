34a5805099097987c14c9499b1867dbeb479104c
Campbell Alden <alden.ca@husky.neu.edu>
2018-02-10 10:53:04 -0500

    Create basic Register view

---
+++ static/src/components/Register.vue
@@ -0,0 +1,76 @@
+<template>
+  <form>
+      <label for='fName'>First Name:</label>
+      <input v-model="fName" id="fName" placeholder="First Name">
+      <br/>
+      <label for='lName'>Last Name:</label>
+      <input v-model="lName" id="lName" placeholder="Last Name">
+      <br/>
+      <label for='uName'>Username:</label>
+      <input v-model="uName" id="uName" placeholder="Username">
+      <br/>
+      <label for='pWord'>Password:</label>
+      <input type="password" id='pWord' v-model="pWord">
+      <br/>
+      <button value="Submit" v-on:click="handleSubmit">Submit</button>
+  </form>
+</template>
+
+<script>
+  import axios from 'axios';
+
+  export default {
+    name: 'login',
+    data () {
+      return {
+        fName: '',
+        lName: '',
+        uName: '',
+        pWord: '',
+      }
+    },
+    methods: {
+      handleSubmit() {
+        let instance = axios.create({
+          baseURL: 'http://localhost:5000/api/'
+        });
+
+        let loginInfo = {
+          firstName: this.fName.value,
+          lastName: this.lName.value,
+          username: this.uName.value,
+          password: this.pWord.value,
+        };
+
+        console.log(loginInfo);
+
+        instance.post('/user/register',
+                      loginInfo)
+        .then(response => {
+          console.log('Got: ' + response.data);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding:  0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
