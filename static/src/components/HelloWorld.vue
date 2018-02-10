7f551f781ce1704358ab8666c482bd9ead1d2ec5
Adrian Kant <adjkant@gmail.com>
2018-02-10 01:29:35 -0500

    minor text fixes

--- static/src/components/HelloWorld.vue
+++ static/src/components/HelloWorld.vue
@@ -9,8 +9,7 @@
 import axios from 'axios';
 
 var instance = axios.create({
-  baseURL: 'http://127.0.0.1:5000/api/',
-  timeout: 1000,
+  baseURL: 'http://127.0.0.1:5000/api/'
 });
 
 export default {
