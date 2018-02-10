a2ca8b9e37782394dde742c604712a5cd9a52f3b
Campbell Alden <alden.ca@husky.neu.edu>
2018-02-10 15:21:24 -0500

    Created endpoints for schools

--- run.py
+++ run.py
@@ -10,6 +10,7 @@ from app.api.database import session_manager
 from app.api.example_api import example_api
 from app.api.user_api import user_api
 from app.api.review_api import review_api
+from app.api.school_api import school_api
 
 # App Declaration
 app = Flask(__name__)
@@ -21,6 +22,7 @@ app.secret_key = 'Hackbeanpot 2018'
 app.register_blueprint(example_api, url_prefix='/api/test')
 app.register_blueprint(user_api, url_prefix='/api/user')
 app.register_blueprint(review_api, url_prefix='/api/review')
+app.register_blueprint(school_api, url_prefix='/api/school')
 
 # Startup
 if __name__ == "__main__":
