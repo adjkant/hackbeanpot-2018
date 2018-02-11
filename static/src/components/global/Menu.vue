c844056cc4bff70f19ad0ef09d9e1bc11a74d5db
Adrian Kant <adjkant@gmail.com>
2018-02-10 22:40:11 -0500

    review flow, slider addition

--- static/src/components/global/Menu.vue
+++ static/src/components/global/Menu.vue
@@ -11,6 +11,7 @@
       <span v-if="$cookies.get('sessionToken')">
         <router-link to="home"><i class="fa fa-home" aria-hidden="true"></i></router-link>
         <router-link to="about"><i class="fa fa-info-circle" aria-hidden="true"></i></router-link>
+        <router-link to="review"><i class="fa fa-plus-square-o" aria-hidden="true"></i></router-link>
         <router-link to="search"><i class="fa fa-search" aria-hidden="true"></i></router-link>
         <router-link to="profile"><i class="fa fa-user" aria-hidden="true"></i></router-link>
         <a href="#"><a v-on:click="logout"><i class="fa fa-sign-out" aria-hidden="true"></i></a></a>
