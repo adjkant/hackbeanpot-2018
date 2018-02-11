2fb7944068d09bad2a90a90d461386a71a41d5e8
Adrian Kant <adjkant@gmail.com>
2018-02-11 11:05:27 -0500

    home

--- static/src/components/pages/Home.vue
+++ static/src/components/pages/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="fixed_center large_card">
     <h1>Hello!</h1>
     <p>Co-ops and internships are great. Super cool. They give students a chance to practice in their field, take advantage of increasingly competitive perks, and open doors to full-time jobs.</p>
 
@@ -10,14 +10,52 @@
     <p>Privacy is key to this process. Users remain anonymous, and reviews aren’t posted until a minimum number of reviews (set by the user) become available.</p>
 
     <p>So go out there! Gain experience! Just don’t forget to share your insights when you return.</p>
-    <router-link to="register">Sign Up</router-link>
-    <router-link to="login">Log In</router-link>
+    <router-link to="login"><b-btn to="register">Sign Up</b-btn></router-link>
+    <router-link to="register"><b-btn class="submit_button" to="login">Log In</b-btn></router-link>
   </div>
 </template>
 
 <style scoped>
 
+  @import '~bootstrap/dist/css/bootstrap.css';
+  @import '~bootstrap-vue/dist/bootstrap-vue.css';
+
+  a a:visited a:hover {
+    color: inherit;
+  }
+
+  .large_card {
+    font-size: 1.5em;
+    color: #333333;
+    font-family: 'Open Sans', Helvetica, Arial, sans-serif;
+    text-align: center;
+  }
+  .large_card_arrows {
+    font-size: 1.5em;
+    width: 80%;
+    color: #333333;
+    font-family: 'Open Sans', Helvetica, Arial, sans-serif;
+    text-align: center;
+  }
+  .fixed_center {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+
+  .submit_button {
+    background-color: #19ab69;
+    border-width: 0px;
+  }
+
+  p {
+    font-size: 18px;
+  }
+
 </style>
+
+
 
 <script>
 
