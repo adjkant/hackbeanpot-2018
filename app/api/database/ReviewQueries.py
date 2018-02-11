3ea8788cf0979ceb45b4d4ca7e0f6365fd918955
Campbell Alden <alden.ca@husky.neu.edu>
2018-02-11 01:22:38 -0500

    Extend review definition

--- app/api/database/ReviewQueries.py
+++ app/api/database/ReviewQueries.py
@@ -2,15 +2,33 @@ from sqlalchemy.exc import SQLAlchemyError
 
 from app.api.database.models.ReviewModel import Review
 
-
 def create_review(db, body):
-  uid = body['user_id']
+  user_id = body['user_id']
   school_id = body['school_id']
   job_id = body['job_id']
   job_type = body['job_type']
   duration = body['duration']
   location = body['location']
-  review = Review(uid, school_id, job_id, job_type, duration, location)
+  salary = body['salary']
+  ratings = body['ratings']
+  min_visible = body['min_visible']
+  show_immediate = body['show_immediate']
+
+  company_id = body['company_id']
+  review_text = body['review_text']
+
+  review = Review(user_id,
+                  school_id,
+                  job_id,
+                  job_type,
+                  company_id,
+                  duration,
+                  location,
+                  salary,
+                  ratings,
+                  min_visible,
+                  show_immediate,
+                  review_text)
 
   try:
     db.add(review)
--- app/api/database/models/ReviewModel.py
+++ app/api/database/models/ReviewModel.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, Enum
+from sqlalchemy import Column, Integer, String, Float, Boolean, ForeignKey, Enum
 from sqlalchemy.orm import relationship
 
 from app.api.database.models.Base import Base
@@ -20,20 +20,51 @@ class Review(Base):
 
   job_id = Column(Integer, ForeignKey('jobs.id'))
   job = relationship('Job', foreign_keys='Review.job_id')
+  job_type = Column(Enum('co-op', 'internship', 'reu', 'remote', name='job_types'))
 
-  job_type = Column(Enum('co-op', 'internship', 'reu', name='job_types'))
+  company_id = Column(Integer, ForeignKey('companys.id'))
+  company = relationship('Company', foreign_keys='Review.company_id')
 
   duration = Column(Integer)
-
   location = Column(String(80))
 
-  def __init__(self, user_id, school_id, job_id, job_type, duration, location):
+  salary = Column(Float)
+
+  culture = Column(Integer)
+  inclusivity = Column(Integer)
+  facilities = Column(Integer)
+  workload = Column(Integer)
+  perks = Column(Integer)
+  opps = Column(Integer)
+  mentors = Column(Integer)
+  purpose = Column(Integer)
+  min_visible = Column(Integer)
+  show_immediate = Column(Boolean)
+
+  review_text = Column(String(1000))
+
+
+  def __init__(self, user_id, school_id, job_id, job_type, company_id, duration, location, salary, ratings, min_visible, show_immediate, review_text):
     self.user_id = user_id
     self.school_id = school_id
     self.job_id = job_id
     self.job_type = job_type
     self.duration = duration
     self.location = location
+    self.salary = salary
+
+    self.culture = ratings[0]
+    self.inclusivity = ratings[1]
+    self.facilities = ratings[2]
+    self.workload = ratings[3]
+    self.perks = ratings[4]
+    self.opps = ratings[5]
+    self.mentors = ratings[6]
+    self.purpose = ratings[7]
+
+    self.min_visible = min_visible
+    self.show_immediate = show_immediate
+    self.review_text = review_text
 
   @property
   def serialize(self):
@@ -43,5 +74,17 @@ class Review(Base):
       'job_id': self.job_id,
       'job_type': self.job_type,
       'duration': self.duration,
-      'location': self.location
+      'location': self.location,
+      'salary': self.salary,
+      'culture': self.culture,
+      'inclusivity': self.inclusivity,
+      'facilities': self.facilities,
+      'workload': self.workload,
+      'perks': self.perks,
+      'opps': self.opps,
+      'mentors': self.mentors,
+      'purpose': self.purpose,
+      'min_visible': self.min_visible,
+      'show_immediate': self.show_immediate,
+      'review_text': self.review_text,
     }
